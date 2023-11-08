39190f5051cf4a8299f2bc43ca2800f843709ce6
backuser3 <backuser3@qr4order.com>
2023-11-08 15:52:11 +0530

    project change

--- qr4order/asgi.py
+++ qr4order/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for restomate project.
+ASGI config for qr4order project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restomate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr4order.settings')
 
 application = get_asgi_application()
--- qr4order/wsgi.py
+++ qr4order/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for restomate project.
+WSGI config for qr4order project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restomate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr4order.settings')
 
 application = get_wsgi_application()
