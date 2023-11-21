ffcc37b0dfa6c728ebb8f111563b0d949ccfd639
Amitkumar Pal <apal@sapat.com>
2023-11-21 18:35:48 +0530

    language change

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path('language/<str:lang>/',LanguageDetails.as_view(),name='translations')
+    path('language/<str:lang>/',LanguageDetails.as_view(),name='translations'),
+    path('language/set/<str:lang>/',LanguageCrud.as_view(),name='translations_cr')
+
 ]
--- api/views.py
+++ api/views.py
@@ -4,14 +4,8 @@ from django.utils import translation
 from django.utils.translation import gettext as _
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
-from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Font, Border, Side
-import pandas as pd
+from rest_framework import status as http_status
 import polib
-import os
-
-
 
 class LanguageDetails(APIView):
 
@@ -34,64 +28,71 @@ class LanguageDetails(APIView):
         for entry in po:
             translations[str(entry.msgid)] = _(str(entry.msgid))
         return JsonResponse(translations)
-
-        
-    # def export(self,request,lang):
-    #     fill = PatternFill(start_color="5983B0", end_color="5983B0", fill_type="solid")
-    #     header_font = Font(bold=True)
-
-    #     msgid_list = []
-    #     msgstr_list = []
-
-    #     po = polib.pofile(f"locale/{lang}/LC_MESSAGES/django.po")
-
-    #     for entry in po:
-    #         msgid_list.append(entry.msgid)
-    #         msgstr_list.append(entry.msgstr)
-
-    #     data = {'Code': msgid_list, 'Phrase': msgstr_list}
-    #     df = pd.DataFrame(data)
-    #     wb = Workbook()
-    #     ws = wb.active
-
-    #     for r in dataframe_to_rows(df, index=False, header=True):
-    #         ws.append(r)
-
-    #     for cell in ws[1]:
-    #         cell.font = header_font
-    #         cell.fill = fill
-
-    #     for row in ws.iter_rows(min_row=2, min_col=2, max_col=2):
-    #         for cell in row:
-    #             cell.alignment = cell.alignment.copy(wrap_text=True)
-
-    #     max_length = 0
-    #     for row in ws.iter_rows(min_row=2, min_col=2, max_col=2):
-    #         for cell in row:
-    #             if cell.value:
-    #                 cell_length = len(str(cell.value))
-    #                 if cell_length > max_length:
-    #                     max_length = cell_length
-    #     ws.column_dimensions['B'].width = max_length + 2
-
-    #     wb.save('your_existing_workbook.xlsx')
-
-    #     # excel_file_path = 'path/to/output/file.xlsx'
     
-    # def post(self,request,lang):
-    #     if not self.checkLang(lang):
-    #         return JsonResponse({'message':"Language not present in the system"})
-        
-    #     res = self.export(request,lang)
+class LanguageCrud(APIView):
 
-    #     # file_path = os.path.join(settings.MEDIA_ROOT, lang)
-    #     # print(file_path)
-    #     # if os.path.exists(file_path):
-    #     #     with open(file_path, 'w') as file:
-    #     #         response = HttpResponse(file.write(), content_type='application/vnd.ms-excel')
-    #     #         response['Content-Disposition'] = f'attachment; filename="{os.path.basename(file_path)}"'
-    #     #         return response
-    #     # else:
-    #     return JsonResponse({"message": "Report not found."})            
+    def checkLang(self,lang):
+        if lang in[code for code, _ in settings.LANGUAGES]:
+            return True
+        return False
+
+    def get(self,request,lang):
+
+        if not self.checkLang(lang):
+            return JsonResponse({'message':"Language not present in the system"},http_status.HTTP_404_NOT_FOUND)
+        
+        translation.activate(lang)
+        translations = {}
+        lang_clean = "en_US" if lang == "en-us" else lang
+        
+        po = polib.pofile(f"locale/{lang_clean}/LC_MESSAGES/django.po")
+
+        for entry in po:
+            translations[str(entry.msgid)] = _(str(entry.msgid))
+        return JsonResponse(translations)
+   
+    def post(self,request,lang):
+
+        if not self.checkLang(lang):
+            return JsonResponse({'message':"Language not present in the system"},http_status.HTTP_404_NOT_FOUND)
+
+        data = request.data        
+        po_file=polib.pofile(f"D:/qr4order_new/qr4v2_django_back/locale/{lang}/LC_MESSAGES/django.po")
+
+        try:
+            for msgid, msgstr in data.items():
+                po_obj = po_file.find(msgid)
+                if po_obj:
+                    po_obj.msgstr = msgstr                    
+                else:
+                    new_entry = polib.POEntry(msgid=msgid, msgstr=msgstr)
+                    po_file.append(new_entry)
+            po_file.save()
+            po_file.save_as_mofile(f"D:/qr4order_new/qr4v2_django_back/locale/{lang}/LC_MESSAGES/django.mo")
+            return JsonResponse({'success': True, 'message': 'Translation Updated Successfully'},http_status.HTTP_200_OK)
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': 'Error creating and updating translations'},http_status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self,request,lang):
+
+        if not self.checkLang(lang):
+            return JsonResponse({'message':"Language not present in the system"},http_status.HTTP_404_NOT_FOUND)
+        
+        data=request.data
+        po_file=polib.pofile(f"D:/qr4order_new/qr4v2_django_back/locale/{lang}/LC_MESSAGES/django.po")
+
+        try:
+            for msgid,msgstr in data.items():
+                po_obj=po_file.find(msgid)
+                if po_obj:
+                    po_obj.obsolete=False
+
+                    
+            po_file.save()
+            po_file.save_as_mofile(f"D:/qr4order_new/qr4v2_django_back/locale/{lang}/LC_MESSAGES/django.mo")
+
+            return JsonResponse({'success': True, 'message': 'Remove Updated Successfully'},http_status.HTTP_200_OK)
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': 'Error creating and updating translations'},http_status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
