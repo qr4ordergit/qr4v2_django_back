abeb9263ff0fb66b0c9e3fc015b8ad1cb13c07e0
neeraj <backuser4@qr4order.com>
2023-11-23 17:44:06 +0530

    rename models

--- restaurant/models.py
+++ restaurant/models.py
@@ -24,27 +24,27 @@ class CommonFields(models.Model):
     class Meta: 
         abstract = True
 
-def generate_restaurant_code():
+def generate_code():
     code = ''.join(choices(ascii_uppercase+digits,k = 8))
-    if Restaurant.objects.filter(referance=code).exists():
-        return generate_restaurant_code
+    if BusinessEntity.objects.filter(referance=code).exists():
+        return generate_code
     return code
 
-class Restaurant(CommonFields):
+
+class BusinessEntity(CommonFields):
     id = models.BigAutoField(primary_key = True)
-    referance = models.CharField(max_length=300,default=generate_restaurant_code)
+    referance = models.CharField(max_length=300,default=generate_code)
     name = models.CharField(max_length=255)
     description = models.TextField(default='',null=True,blank=True)
     status = models.BooleanField(default=False)
     # timezone = models.ForeignKey(Timezone, on_delete=models.CASCADE, related_name='restaurant_timezone', null=True)
     # country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='restaurant_country',default='',null=True,blank=True)
     # currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='restaurant_currency',default='1',null=True, blank=True)
-
     def __str__(self) -> str:
         return self.name
 
-class RestaurantTable(CommonFields):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="restauranttable_restaurant",null=True, blank=True)
+class QrSingature(CommonFields):
+    restaurant = models.ForeignKey(BusinessEntity, on_delete=models.CASCADE, related_name="restauranttable_restaurant",null=True, blank=True)
     table_name = models.CharField(max_length=20,null=True, blank=True)
     qr_code = models.FileField(null=True, blank=True)
     description = models.TextField(default='', null=True, blank=True)
@@ -53,8 +53,15 @@ class RestaurantTable(CommonFields):
     is_online_order = models.BooleanField(default=False)
     bill_type = models.BooleanField(default=False)
     
-
-  
     def __str__(self) -> str:
         return self.table_name
 
+
+#table session
+# class OrderSession(models.Model):
+#     pass
+
+#ordersummary
+# class OrderDetails(models.Model):
+#     pass
+    #   status = pending,configme,
