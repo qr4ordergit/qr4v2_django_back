20f793ee353da2d6678f7b69f3d3984fe03bfe91
neeraj <backuser4@qr4order.com>
2023-11-29 11:46:36 +0530

    remove restaurnat

--- qr4order/urls.py
+++ qr4order/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authenticator.urls')),
     path('accounts/', include('allauth.urls')),
-    path('api/',include("restaurant.urls"),name = "api"),
     path('multistore/',include('multistore.urls')),
 ]
