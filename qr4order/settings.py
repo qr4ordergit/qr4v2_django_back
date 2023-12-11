7ade3c5e13f36cd5d33d0bf220b379bbe1b2c727
rupava <rbaruah@sapat.com>
2023-12-11 10:34:13 +0530

    settings push

--- qr4order/settings.py
+++ qr4order/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'qr4order.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MSQL_USER_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': '3306',
     }
 }
 
